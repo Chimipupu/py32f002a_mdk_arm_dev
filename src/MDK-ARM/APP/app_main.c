e093df103732851927f7a8bd1b5345a611ad94f4
Chimipupu <chiminyanyan@gmail.com>
2025-09-11 22:27:07 +0900

    ステートマシーンを実装

--- src/MDK-ARM/APP/app_main.c
+++ src/MDK-ARM/APP/app_main.c
@@ -11,8 +11,23 @@
 #include "app_main.h"
 #include "main.h"
 
+#define SM_TX_DATA_BUF_SIZE     64
+
+typedef enum {
+    SM_ERROR        = 0xFF, // エラー状態
+    SM_NONE         = 0x00, // 状態なし
+    SM_IDLE         = 0x10, // アイドル状態
+    SM_BUSY         = 0x20, // ビジー状態
+    SM_PROC_END     = 0x30, // 処理完了状態
+    SM_MATH_CALC    = 0x40, // 円周率計算状態
+} e_state_machine;
+
 static float s_math_pi;
-uint8_t s_tx_data_buf[16] = {0};
+static uint8_t s_tx_data_buf[SM_TX_DATA_BUF_SIZE] = {0};
+static e_state_machine s_state = SM_NONE;
+
+static void sm_init(void);
+static void sm_main(void);
 
 // ガウス・ルジャンドル法による円周率の計算
 float app_math_pi_calc(uint32_t cnt)
@@ -41,13 +56,55 @@ float app_math_pi_calc(uint32_t cnt)
 }
 
 /**
+ * @brief ステートマシーン初期化
+ * 
+ */
+void sm_init(void)
+{
+    s_state = SM_MATH_CALC;
+}
+
+/**
+ * @brief ステートマシーン メイン
+ * 
+ */
+void sm_main(void)
+{
+    switch (s_state)
+    {
+        case SM_ERROR:
+            break;
+
+        case SM_BUSY:
+            break;
+
+        case SM_PROC_END:
+            printf("%s\r\n", s_tx_data_buf);
+            memset(s_tx_data_buf, 0x00, sizeof(s_tx_data_buf));
+            s_state = SM_IDLE;
+            break;
+
+        case SM_MATH_CALC:
+            s_math_pi = app_math_pi_calc(4);
+            sprintf((char *)&s_tx_data_buf[0], "pi = %f", s_math_pi);
+            s_state = SM_PROC_END;
+            break;
+
+        case SM_NONE:
+        case SM_IDLE:
+        default:
+            s_state = SM_MATH_CALC;
+            break;
+    }
+}
+
+/**
  * @brief アプリメイン初期化
  * 
  */
 void app_main_init(void)
 {
     memset(s_tx_data_buf, 0x00, sizeof(s_tx_data_buf));
-    s_math_pi = app_math_pi_calc(4);
 }
 
 /**
@@ -56,6 +113,5 @@ void app_main_init(void)
  */
 void app_main(void)
 {
-    sprintf((char *)&s_tx_data_buf[0], "pi = %f", s_math_pi);
-    printf("%s\r\n", s_tx_data_buf);
+    sm_main();
 }
